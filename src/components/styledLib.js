1a73cd545bd462984eef08fe3fac8f7974fc9d9a
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-08-05 22:23:58 -0500

    add style library

---
+++ src/components/styledLib.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const devices = {
+    tablet: '768px',
+    laptop: '1224px',
+    wide: '1800px'
+}
+
+export const Wrapper = styled.div`
+    max-width: 1000px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 1rem;
+`
