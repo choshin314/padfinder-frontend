04dd43cb3e401eed297288a2b2aa5d3935eb55d5
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-08-10 15:30:19 -0500

    add Main content wrapper for top-level

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import styled from 'styled-components'
 
 import {MapContextProvider} from './context/MapContext'
 import MainNav from './components/nav/MainNav'
 import Home from './pages/Home'
 import MapView from './pages/MapView'
 import PropertyView from './pages/PropertyView'
+import Authentication from './pages/Authentication'
 
 function App() {
   
@@ -13,14 +15,22 @@ function App() {
     <MapContextProvider>
       <Router>
         <MainNav />
-        <Switch>
-          <Route exact path="/"><Home /></Route>
-          <Route exact path="/search/:searchquery"><MapView /></Route>
-          
-        </Switch>
-      </Router>
+        <Main>
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route exact path="/authenticate"><Authentication /></Route>
+            <Route exact path="/search/:searchquery"><MapView /></Route>  
+          </Switch>
+        </Main>
+      </Router>  
     </MapContextProvider>
   );
 }
 
 export default App;
+
+const Main = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-top: 4rem;
+`
