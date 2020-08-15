094d7b847dc9dd09f288da494c4b2156a043be46
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-08-14 23:23:06 -0500

    swap in useReducer for ImgSlider touch state

--- src/components/shared/ImgSlider.js
+++ src/components/shared/ImgSlider.js
@@ -1,27 +1,17 @@
 import React, {useState, useReducer} from 'react'
 import styled from 'styled-components'
 
-// function reducer(state, action) {
-//     switch(action.type) {
-//         case "NEXT_SLIDE": { 
-//             return ( state.currentSlide < action.arrLength ? 
-//                 { currentSlide: currentSlide + 1 } : 
-//                 { currentSlide: 0 }
-//             )  
-//         };
-//         case "PREV_SLIDE": {
-//             return ( state.currentSlide > 0 ?
-//                 { currentSlide: currentSlide -1 } :
-//                 { currentSlide: action.arrLength - 1 }
-//             )
-//         }
-//     }
-// }
+function reducer(state, action) {
+    switch(action.type) {
+        case "START_SWIPE": return { isTracking: true, startingX: action.value };
+        case "RESET": return { isTracking: false, startingX: null };
+    }
+}
 
-const ImgSlider = ({images}) => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [startingX, setStartingX] = useState(null);
-    const [isTracking, setIsTracking] = useState(false);
+const ImgSlider = ({images, startingSlide}) => {
+    let initSlide = startingSlide ? startingSlide : 0;
+    const [currentSlide, setCurrentSlide] = useState(initSlide);
+    const [{ isTracking, startingX }, dispatch] = useReducer(reducer, { isTracking: false, startingX: null })
 
     //need to apply these to both button click and swipes
     function nextSlide() {
@@ -36,21 +26,18 @@ const ImgSlider = ({images}) => {
         setCurrentSlide(images.length - 1)
     }
 
-    const handleSwipeStart = (e) => {
+    function handleSwipeStart(e) {
         const initialX = e.touches[0].clientX;
-        setIsTracking(true);
-        setStartingX(initialX);
+        dispatch({ type: "START_SWIPE", value: initialX});
     }
 
-    const handleSwiping = (e) => {
+    function handleSwiping(e) {
         if (!isTracking) return;
-
         const currentX = e.touches[0].clientX;
         let diffX = Math.abs(currentX - startingX);
         if (diffX < 50) return; //weed out tiny accidental touches
         currentX > startingX ? prevSlide() : nextSlide();
-        setIsTracking(false);
-        setStartingX(null);
+        dispatch({ type: "RESET" });
         e.preventDefault();
     }
 
@@ -60,10 +47,15 @@ const ImgSlider = ({images}) => {
             onTouchMove={handleSwiping}
         >
             <SliderTrack currentSlide={currentSlide}>
-                {images.map((image, index) =>  { return (
-                    <SliderItem key={`image${index}`}>
-                        <img src={image.src} />
-                    </SliderItem>    )                   
+                {images.map((image, index) =>  { 
+                    return (
+                        <SliderItem key={`image${index}`}>
+                            <img src={image.src} />
+                            <SliderItemNum>
+                                <span>{`${currentSlide + 1} of ${images.length}`}</span>
+                            </SliderItemNum>
+                        </SliderItem>    
+                    )                   
                 })}
             </SliderTrack>
         </SliderFrame>
@@ -89,9 +81,21 @@ const SliderTrack = styled.div`
 const SliderItem = styled.div`
     width: 100%;
     flex: 1 0 100%;
+    position: relative;
     & > img {
         width: 100%;
         object-fit: cover;
         pointer-events: none;
     }
+`
+const SliderItemNum = styled.div`
+    width: 20%;
+    min-width: 50px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    background: rgba(0,0,0,.2);
+    color: white;
+    text-align: center;
+    line-height: 1.5;
 `
