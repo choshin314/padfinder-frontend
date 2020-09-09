c234c6642daf09fd6cd158a5b99988a7fddcc59c
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-09-08 19:07:41 -0500

    Replace useForm with everything from useFormSimple, got rid of useFormSimple

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,66 +1,46 @@
-import {useReducer} from 'react'
+import {useState} from 'react'
 
-export const useForm = (initialState) => {
+export const useForm = (initialState, submitCallback, validateForm) => {
+    const [inputValues, setInputValues] = useState(initialState);
+    const [inputErrors, setInputErrors] = useState({});
+    const [otherErrors, setOtherErrors] = useState(null);
 
-    function reducer(state, action) {
-        switch(action.type) {
-            case "CHANGE_INPUT": return { ...state, [action.key]: action.value };
-            case "CHANGE_MINMAX_INPUT": {
-                return ( action.min ? 
-                    { 
-                        ...state, 
-                        [action.key]: {
-                            ...state[action.key],
-                            [action.subKey]:  [ action.value, state[action.key][action.subKey][1] ]
-                        }
-                    } :
-                    { 
-                        ...state, 
-                        [action.key]: {
-                            ...state[action.key],
-                            [action.subKey]: [ state[action.key][action.subKey][0], action.value ]
-                        }
-                    }
-                )
-            };
-            case "CHANGE_NESTED_INPUT": return { 
-                ...state, 
-                [action.key]: {
-                    ...state[action.key],
-                    ...action.updateSubKey
-                }
-            }
-            case "RESET": return { ...initialState };
-        }
-    };
-    
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    function handleInputChange(e, array) {
+    function handleChange(e) {
         let { name, value, checked, type } = e.target;
         if (type === "checkbox") { value = checked };
-        array ?
-            dispatch({ type: "CHANGE_INPUT", key: name, value: [value, value] }) :
-            dispatch({ type: "CHANGE_INPUT", key: name, value: value })
-    }       
-    // param 'array' is for properties that can potentially have minmax ranges (rent, beds, baths, size) - if they DO have ranges, they'll be handled by handleMinMaxChange, but if they DON'T have ranges, I still wanna keep the value as an array just to keep things simpler.  The array values in this case would just be [same, same]
-
-    function handleInputChangeNested(e, topState, array=false) {
-        let { value, name, type } = e.target;
-        if (type === "number") { value = parseFloat(value) }; 
-        if (value === "true" || value === "false") { value = JSON.parse(value) }; //option input converts everything to strings.  Need to convert back to bool.
-        array ? 
-            dispatch({type: "CHANGE_NESTED_INPUT", key: topState, updateSubKey: { [ name ]: [ value, value ] }}) :
-            dispatch({type: "CHANGE_NESTED_INPUT", key: topState, updateSubKey: { [ name ]: value }})
+        setInputValues({
+            ...inputValues,
+            [name]: value
+        })
     }
 
-    function handleMinMaxChange(e, topState, min) {
-        const { value, name } = e.target;
-        dispatch({ type: "CHANGE_MINMAX_INPUT", key: topState, subKey: name, min: min, value: parseFloat(value) })
+    function validateAndSubmit(e) {
+        e.preventDefault();
+        let errors = {};
+        //run validation only if a validation argument was passed into the hook
+        if (validateForm) {
+            errors = validateForm(inputValues); //returns errors object
+            setInputErrors(errors);
+        } 
+        //if no errors, submit form, else exit operation and display errors
+        if(Object.keys(errors).length === 0) {
+            submitCallback(inputValues)
+        }
+    }
+    //submitCallback is passed in as param to this hook - contains any form-specific submit handling logic
+    
+    function resetForm() {
+        setInputValues({...initialState})
     }
 
-    return [state, dispatch, handleInputChange, handleInputChangeNested, handleMinMaxChange];
+    return { 
+        inputValues, 
+        setInputValues,
+        inputErrors, 
+        handleChange,
+        validateAndSubmit, 
+        otherErrors, 
+        setOtherErrors, 
+        resetForm 
+    }
 }
-
-//basically handleInputChange should work for all text-ish inputs (text, email, phone, textarea) and checkboxes
-//handleInputChange will NOT work for the datepicker since datepicker doesn't give direct access to event. Will need to dispatch custom action
