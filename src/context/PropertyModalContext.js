aaf6ce5c234a634d9e975a6a364f5ab1d45494bb
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-09-09 23:29:47 -0500

    add localStorage persistence for expandedProperty to fix PropertyUpdate breaking on refresh

--- src/context/PropertyModalContext.js
+++ src/context/PropertyModalContext.js
@@ -2,16 +2,38 @@ import React, {useState} from 'react'
 
 const PropertyModalContext = React.createContext()
 
+function checkLocalStorage(key, defaultValue) {
+    let localItem = localStorage.getItem(key);
+    return ( localItem ? JSON.parse(localItem) : defaultValue );
+}
+
 const PropertyModalContextProvider = (props) => {
-    const [expandedProperty, setExpandedProperty] = useState(null);
+    const [expandedProperty, setExpandedProperty] = useState(checkLocalStorage('expandedProperty', null));
     const [modalOpen, setModalOpen] = useState(false);
 
-    const toggleModal = () => setModalOpen(!modalOpen);
+    const toggleModal = () => {
+        if (modalOpen) {
+            localStorage.removeItem('expandedProperty')
+        }
+        setModalOpen(!modalOpen);
+    }
+
+    const propertyMethods = {
+        openProperty: function(property) {
+            setExpandedProperty(property);
+            localStorage.setItem('expandedProperty', JSON.stringify(property))
+        },
+        closeProperty: function(property) {
+            setExpandedProperty(null);
+            localStorage.removeItem('expandedProperty');
+        }
+    }
 
     return (
         <PropertyModalContext.Provider value={{
             expandedProperty, 
             setExpandedProperty,
+            propertyMethods,
             modalOpen,
             toggleModal,
             setModalOpen
