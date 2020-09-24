5ac64c6d461e08cdc09c72818d0d031fe301670d
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-09-23 20:49:49 -0500

    add success handler for ContactForm, also more validation

--- src/components/properties/ContactForm.js
+++ src/components/properties/ContactForm.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import styled from 'styled-components'
 
 import {useForm} from '../../hooks/useForm'
@@ -8,15 +8,18 @@ import FormTextArea from '../formElements/FormTextArea'
 import FormDatePicker from '../formElements/FormDatePicker'
 import Card from '../shared/Card'
 import {PropertyContext} from '../../context/PropertyContext'
+import {convertDateToMMDDYYYY} from '../../helpers'
 
 const validateForm = values => {
     let errors = {}
-    if (!/^\S+@\S+\.\S+$/.test(values.email)) {
+    if (!values.name) errors.name = "Name required";
+    if (!values.email || !/^\S+@\S+\.\S+$/.test(values.email)) {
         errors.email = "Valid email address required"
-    }
-    if (!/^\d{10}$/.test(values.phone)) {
+    };
+    if (!values.phone || !/^\d{10}$/.test(values.phone)) {
         errors.phone = "10-digit phone number required (numbers only)"
-    }
+    };
+    if (!values.moveInDate) errors.moveInDate = "Move-In Date required";
     return errors;
 }
 
@@ -29,10 +32,9 @@ const ContactForm = props => {
         phone: '',
         message: `I'm interested in your property at ${expandedProperty.address.street}.  Please send me more information!`
     }
-    const { inputValues, setInputValues, inputErrors, handleChange, validateAndSubmit, otherErrors, setOtherErrors, resetForm } = useForm(initialState, submitCallback, validateForm);
+    const { inputValues, setInputValues, inputErrors, handleChange, validateAndSubmit, otherErrors, setOtherErrors, resetForm } = useForm(initialState, handleSubmit, validateForm);
     const { name, email, moveInDate, phone, message } = inputValues;
-    
-    //^^ probably gonna need this later so I can send property listing agent userID + propertyId on form submit
+    const [ success, setSuccess ] = useState(false);
 
     function handleDateChange(date) {
         setInputValues({
@@ -41,8 +43,26 @@ const ContactForm = props => {
         })
     }
 
-    function submitCallback() {
-        console.log('Sending to backend for email')
+    async function handleSubmit() {
+        setSuccess(false)
+        try {
+            const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/api/properties/inquiry`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ 
+                    ...inputValues,
+                    moveInDate: convertDateToMMDDYYYY(moveInDate),
+                    propertyId: expandedProperty._id
+                })
+            });
+            const data = await response.json();
+            if (response.status !== 200) throw new Error(data.message);
+            setSuccess(true)
+        } catch(err) {
+           setOtherErrors(err.message);
+        } 
         resetForm();
     }
 
@@ -51,7 +71,7 @@ const ContactForm = props => {
             <FormHeader>
                 <h2>Contact Property Manager</h2>
             </FormHeader>
-            <Form onSubmit={validateAndSubmit}>
+            <Form onSubmit={validateAndSubmit} noValidate>
                 <FormInput
                     type="text"
                     id="contactName"
@@ -110,6 +130,8 @@ const ContactForm = props => {
                     value={message}
                 />
                 <FormButton>Send Message</FormButton>
+                {otherErrors && <FormError>{otherErrors}</FormError>}
+                {success && <FormSuccess>Message sent!</FormSuccess>}
             </Form>
         </Card>
     )
@@ -144,4 +166,16 @@ const SplitDiv = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     column-gap: .5rem;
+`
+const FormError = styled.div`
+    text-align: center;
+    padding: .5rem;
+    color: red;
+    font-weight: bold;
+    font-size: 1rem;
+    width: 100%;
+`
+
+const FormSuccess = styled(FormError)`
+    color: var(--primary-color);
 `
