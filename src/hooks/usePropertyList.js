b7eee524dfbd0b6921abe1501c01e3d922e9d9a7
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-09-24 23:35:05 -0500

    add usePropertylist hook to take care of fetching and managing property lists

---
+++ src/hooks/usePropertyList.js
@@ -0,0 +1,37 @@
+import {useState, useContext} from 'react'
+import {AuthContext} from '../context/AuthContext'
+
+export const usePropertyList = () => {
+    const {user} = useContext(AuthContext);
+    const [ pagination, setPagination ] = useState({});
+    const [ errorMsg, setErrorMsg ] = useState(null)
+
+    async function fetchPropertyList(listName, pageNum = 1, limit = 100, setPropertyList) {
+        if (user) {
+            try {
+                const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/api/properties/list/${listName}/${user.userId}?pg=${pageNum}&limit=${limit}`, {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${user.token}`
+                    }
+                });
+                if (response.status !== 200) throw new Error('Could not retrieve properties.  Try again later.');
+                const data = await response.json();
+                if (data.properties.length < 1) throw new Error('No properties found.');
+                setPropertyList(data.properties);
+                setPagination({
+                    totalPages: data.totalPages,
+                    currentPage: data.currentPage,
+                    prevPage: data.prevPage,
+                    nextPage: data.nextPage
+                })
+                console.log(data.properties);
+            } catch(err) {
+                return setErrorMsg(err.message);
+            }
+        }
+    }
+
+    return { fetchPropertyList, pagination, errorMsg }
+}
+
